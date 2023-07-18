455b5c68b1733d7ca80d5d10cad1380639e0ec01
Apurbaa <cse21029@cemk.ac.in>
2023-07-19 00:07:21 +0530

    Multiply property added

--- sum.java
+++ sum.java
@@ -2,5 +2,6 @@ public class sum{
     public static void main(String args[]){
         int a=5,b=6;
         System.out.println("Sum of "+a+"and"+b+"is"+a+b);
+        System.out.println("Product "+a+"and"+b+"is"+a*b);
     }
 }
