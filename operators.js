c691b3504d6c4ce95a078ae6e713b8397dea0d5c
Apurbaa <cse21029@cemk.ac.in>
2023-07-22 15:52:03 +0530

    Operator JavaScript file added

---
+++ operators.js
@@ -0,0 +1,32 @@
+var x=3!==3;//comparison operator
+x
+var a=10,b=2;//exponent
+c=a**b;
+c
+var x=7>5;//compare
+x
+var z=3<=3;
+z
+var y=5,z=3;//increment decrement
+y++;
+y
+z--;
+z
+var d=true;//converts true to positive integer no 
+d=+d;
+d
+var p=false;//converts false to positive integer no
+p=+p;
+p
+var t="7";//converts string to positive integer no
+t=+t;
+t
+var r="8";//converts string to negative integer no
+r=-r;
+r
+var w=true;//converts true to negative integer no 
+w=-w;
+w
+var str1="Hello ",str2="World";
+str3=str1+str2;
+str3
